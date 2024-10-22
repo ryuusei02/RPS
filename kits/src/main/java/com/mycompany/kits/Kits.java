669c97acd65debb63a71f78a86ca64c7e2e90f7d
Pangan, Kit Wendell R. <160462103+ryuusei02@users.noreply.github.com>
2024-10-22 10:26:45 +0800

    Add files via upload

---
+++ kits/src/main/java/com/mycompany/kits/Kits.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.kits;
+
+/**
+ *
+ * @author CL3-PC24
+ */
+public class Kits {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
